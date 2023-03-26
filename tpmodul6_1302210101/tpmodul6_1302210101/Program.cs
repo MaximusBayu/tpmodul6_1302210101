7ce98ec44cf64fa017dbd6bd6a89d703521dbb37
MaximusBayu <121425228+MaximusBayu@users.noreply.github.com>
2023-03-26 13:04:15 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_1302210101/tpmodul6_1302210101/Program.cs
+++ tpmodul6_1302210101/tpmodul6_1302210101/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using tpmodul6_1302210101;
+
+SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - Maximus");
+Console.WriteLine("Masukkan Berapa Kali Video Akan Dimainkan: ");
+int play = Convert.ToInt32(Console.ReadLine());
+video.IncreasePlayCount(play);
+video.PrintVideoDetails();
+
---
+++ tpmodul6_1302210101/tpmodul6_1302210101/SayaTubeVideo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul6_1302210101
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(String title) {
+            Random random = new Random();
+            this.id = random.Next(100000);
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int play) {
+            this.playCount = play;
+        }
+
+        public void PrintVideoDetails() {
+            Console.WriteLine("ID\t\t:"+id);
+            Console.WriteLine("Judul\t\t:"+title);
+            Console.WriteLine("Jumlah Diputar\t:"+playCount);
+        }
+    }
+}
