8f2c55bcbd4aaa82a4110f610594c213eaf635fc
MaximusBayu <121425228+MaximusBayu@users.noreply.github.com>
2023-03-26 14:02:03 +0700

    implementasi design by contract

--- tpmodul6_1302210101/tpmodul6_1302210101/Program.cs
+++ tpmodul6_1302210101/tpmodul6_1302210101/Program.cs
@@ -5,5 +5,10 @@ SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - Maximus")
 Console.WriteLine("Masukkan Berapa Kali Video Akan Dimainkan: ");
 int play = Convert.ToInt32(Console.ReadLine());
 video.IncreasePlayCount(play);
+
+for (int i = 0; i < 1000; i++)
+{
+    video.IncreasePlayCount(10000000);
+}
 video.PrintVideoDetails();
 
--- tpmodul6_1302210101/tpmodul6_1302210101/SayaTubeVideo.cs
+++ tpmodul6_1302210101/tpmodul6_1302210101/SayaTubeVideo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,14 +14,29 @@ namespace tpmodul6_1302210101
         private int playCount;
 
         public SayaTubeVideo(String title) {
+
+            Contract.Requires(this.title != null, "Judul video tidak boleh kosong");
+            Contract.Requires(this.title.Length <= 100, "Judul video maksimal 100 karakter");
             Random random = new Random();
             this.id = random.Next(100000);
             this.title = title;
             this.playCount = 0;
+            
         }
-
+        
         public void IncreasePlayCount(int play) {
-            this.playCount = play;
+            Contract.Requires(playCount >= 0 && playCount <= 10000000, "Input 0 hingga 10.000.000");
+            try
+            {
+                checked
+                {
+                    this.playCount += play;
+                }
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
 
         public void PrintVideoDetails() {
